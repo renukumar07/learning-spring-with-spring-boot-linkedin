bd2bccba4b6b7cc1fd9e3b83b4517001465aff93
renukumar07 <renukumar90@gmail.com>
2022-05-27 22:43:43 +0530

    Added guest, reservation entity and repository classes and printing data

--- src/main/java/com/renukumar/learningspring/util/AppStartupEvent.java
+++ src/main/java/com/renukumar/learningspring/util/AppStartupEvent.java
@@ -1,5 +1,9 @@
 package com.renukumar.learningspring.util;
 
+import com.renukumar.learningspring.data.Guest;
+import com.renukumar.learningspring.data.GuestRepository;
+import com.renukumar.learningspring.data.Reservation;
+import com.renukumar.learningspring.data.ReservationRepository;
 import com.renukumar.learningspring.data.Room;
 import com.renukumar.learningspring.data.RoomRepository;
 
@@ -10,13 +14,24 @@ import org.springframework.stereotype.Component;
 @Component
 public class AppStartupEvent implements ApplicationListener<ApplicationReadyEvent> {
   private RoomRepository roomRepository;
-  public AppStartupEvent(RoomRepository roomRepository){
+  private GuestRepository guestRepository;
+  private ReservationRepository reservationRepository;
+
+  public AppStartupEvent(RoomRepository roomRepository, GuestRepository guestRepository, ReservationRepository reservationRepository){
     this.roomRepository = roomRepository;
+    this.guestRepository = guestRepository;
+    this.reservationRepository =  reservationRepository;
   }
   @Override
   public void onApplicationEvent(ApplicationReadyEvent event) {
     Iterable<Room> rooms = this.roomRepository.findAll();
     rooms.forEach(System.out::println);
+
+    Iterable<Guest> guests = this.guestRepository.findAll();
+    guests.forEach(System.out::println);
+
+    Iterable<Reservation> reservations = this.reservationRepository.findAll();
+    reservations.forEach(System.out::println);
   }
 
   
